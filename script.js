084cca8e7d3b653046a58f42b9776095ccb770d2
willl85 <ngowilfried@gmail.com>
2023-09-11 19:12:35 +0200

    Mouse Events and Drawing

--- script.js
+++ script.js
@@ -7,9 +7,39 @@ const ctx = canvas.getContext('2d');
 
 let size = 20;
 let color = 'black';
-
+let isPressed = false;
 let x;
 let y;
+
+canvas.addEventListener('mousedown', (e) => {
+  isPressed = true;
+
+  x = e.offsetX;
+  y = e.offsetY;
+
+});
+
+canvas.addEventListener('mouseup', (e) => {
+  isPressed = false;
+
+  x = undefined;
+  y = undefined;
+});
+
+canvas.addEventListener('mousemove', (e) => {
+  if(isPressed) {
+    const x2 = e.offsetX;
+    const y2 = e.offsetY;
+    
+    drawCircle(x2, y2);
+    drawLine(x, y, x2, y2);
+
+    x = x2;
+    y = y2;
+
+    console.log(x, y, x2, y2);
+  }
+});
 
 function drawCircle(x, y) {
   ctx.beginPath();
@@ -18,11 +48,11 @@ function drawCircle(x, y) {
   ctx.fill();
 }
 
-function drawLine(x1, x2, y1, y2) {
+function drawLine(x1, y1, x2, y2) {
   ctx.beginPath();
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
   ctx.strokeStyle = color;
-  ctx.lineWidth = size;
+  ctx.lineWidth = size * 2;
   ctx.stroke();
 }
